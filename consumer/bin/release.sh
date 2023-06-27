ca302cd7ab75fd0fe991d9987ebbc71325a69814
DavidAmmeraal <d.ammeraal@quin.md>
2023-06-27 15:23:40 +0200

    add a breaking change

---
+++ consumer/bin/release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export PACT_BROKER_BASE_URL="http://localhost:9292"
+
+DEFAULT_VERSION=$(git rev-parse --short HEAD)
+
+if [ -z ${VERSION+x} ];
+then
+	VERSION=$DEFAULT_VERSION
+else
+	echo "Checking if we can deploy $VERSION"
+fi
+
+docker run --rm \
+ --network="host" \
+ -w ${PWD} \
+ -v ${PWD}:${PWD} \
+ -e PACT_BROKER_BASE_URL \
+ -e PACT_BROKER_TOKEN \
+  pactfoundation/pact-cli:latest \
+  pact-broker record-release \
+	--pacticipant=MyConsumer \
+	--environment=$RELEASE_ENV\
+  --version=$VERSION
