e81c98c02fe75a64f855cef93ee48756022a02be
DavidAmmeraal <d.ammeraal@quin.md>
2023-06-28 14:40:44 +0200

    added delete todo

--- consumer/src/api/__tests__/todos.contract.spec.ts
+++ consumer/src/api/__tests__/todos.contract.spec.ts
@@ -55,69 +55,75 @@ describe("GET /todos", () => {
   });
 });
 
-xdescribe("DELETE /todos/:id", () => {
-  beforeEach(() => {
-    provider
-      // This describes the server state at the moment of the request
-      .given("a todo with id 1 exists")
-      .uponReceiving("a request to delete todo with id 1")
-      .withRequest({
-        method: "DELETE",
-        path: "/todos/1",
-      })
-      .willRespondWith({
-        status: 204,
-      });
-  });
-
-  it("return an HTTP 204", async () => {
-    await provider.executeTest(async (mockServer) => {
-      const todosService = new ToDosApi({ baseUrl: mockServer.url });
-
-      const result = await todosService.deleteTodoById("1");
-      expect(result).toBe(true);
-    });
-  });
-});
-
 const describeIf = (condition: boolean) => (condition ? describe : xdescribe);
 const xDescribeIf = (condition: boolean) => xdescribe;
 
-describe("POST /todos", () => {
-  beforeEach(() => {
-    provider
-      // This describes the server state at the moment of the request
-      .uponReceiving("a request to create a new todo")
-      .withRequest({
-        method: "POST",
-        path: "/todos",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: {
-          description: MatchersV3.like("some todo description"),
-        },
-      })
-      .willRespondWith({
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: {
-          id: MatchersV3.like("some-id"),
-        },
-      });
-  });
-
-  it("responds with HTTP 200", async () => {
-    await provider.executeTest(async (mockServer) => {
-      const todosService = new ToDosApi({ baseUrl: mockServer.url });
-
-      const result = await todosService.createTodo({
-        description: "some todo description",
-      });
-      expect(result.id).toBe("some-id");
+describeIf(featureFlags.getFeatureFlag("contract_delete_todo"))(
+  "DELETE /todos/id",
+  () => {
+    beforeEach(() => {
+      provider
+        // This describes the server state at the moment of the request
+        .given("a todo with id 1 exists")
+        .uponReceiving("a request to delete todo with id 1")
+        .withRequest({
+          method: "DELETE",
+          path: "/todos/1",
+        })
+        .willRespondWith({
+          status: 204,
+        });
     });
-  });
-});
+
+    it("return an HTTP 204", async () => {
+      await provider.executeTest(async (mockServer) => {
+        const todosService = new ToDosApi({ baseUrl: mockServer.url });
+
+        const result = await todosService.deleteTodoById("1");
+        expect(result).toBe(true);
+      });
+    });
+  }
+);
+
+describeIf(featureFlags.getFeatureFlag("contract_create_todo"))(
+  "POST /todos",
+  () => {
+    beforeEach(() => {
+      provider
+        // This describes the server state at the moment of the request
+        .uponReceiving("a request to create a new todo")
+        .withRequest({
+          method: "POST",
+          path: "/todos",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: {
+            description: MatchersV3.like("some todo description"),
+          },
+        })
+        .willRespondWith({
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: {
+            id: MatchersV3.like("some-id"),
+          },
+        });
+    });
+
+    it("responds with HTTP 200", async () => {
+      await provider.executeTest(async (mockServer) => {
+        const todosService = new ToDosApi({ baseUrl: mockServer.url });
+
+        const result = await todosService.createTodo({
+          description: "some todo description",
+        });
+        expect(result.id).toBe("some-id");
+      });
+    });
+  }
+);
